9fb173de5f51a8db2b63efc1e014f0f1bc4e82a8
PawanPatel951 <pawanpatelrj19@gmail.com>
2026-03-22 19:34:51 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import { memo, useMemo, useState } from 'react'
+import './App.css'
+
+function App() {
+  const [searchTerm, setSearchTerm] = useState("");
+  const items = [
+  "🍎 Apple",
+  "🍌 Banana",
+  "🍒 Cherry",
+  "🍊 Orange",
+  "🍇 Grapes",
+  "🥭 Mango"
+];
+
+  const handlechange = (e) => {
+    setSearchTerm(e.target.value);
+  }
+
+  const filteredItems = useMemo(() => {
+    return items.filter((item) =>
+      item.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+  } , [searchTerm]);
+
+  return (
+    <>
+      <div className="App">
+       <div className="content">
+        <h1>Fruit List 🍎🍌🍒</h1>
+        <input type="text" placeholder='Search term' value={searchTerm} onChange={handlechange}/>
+
+        <ul>
+          {
+            filteredItems.map((item, index) => (
+            <p key={index}>{item}</p>
+          ))}
+        </ul>
+       </div>
+        </div>
+    </>
+  )
+}
+
+export default App
